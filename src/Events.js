ae93c229716789b5ba26526651a4d3f8686037e0
Akansha0211 <akansha.saxena0211@gmail.com>
2021-06-21 15:00:37 +0530

    Styling the cards component feteched through api

--- src/Events.js
+++ src/Events.js
@@ -5,9 +5,26 @@ import CodeSharpIcon from '@material-ui/icons/CodeSharp';
 import LiveTvSharpIcon from '@material-ui/icons/LiveTvSharp';
 import VideocamSharpIcon from '@material-ui/icons/VideocamSharp';
 import './Events.css';
+import axios from 'axios';
 
 class Events extends Component {
-    state = {  }
+    state = { 
+        events:[]
+     };
+
+     //manage GET requests from API
+     async componentDidMount(){
+         const url = "https://api.codingninjas.com/api/v3/events?event_category=ALL_EVENTS&event_sub_category=Upcoming&tag_list=&offset=0";
+         const response = await axios.get(url);
+         const data = response.data;
+         const datl2 = data.data
+        //  console.log(data);
+         const arr = datl2.events;
+        //  console.log(arr);
+         this.setState({events:arr});
+         console.log(this.state.events);
+     }
+
     render() { 
         return ( 
             <div className="events">
@@ -48,6 +65,90 @@ class Events extends Component {
                         </li>
                     </ul>
                 </nav>
+                {/* {this.state.events.map((event, index)=>{
+                    const{name,short_desc,registration_start_time,fees,venue,mobile_cover_picture} = event;
+                    return(
+                        <div className="container-fluid mt-4">
+                            <div className="row">
+                                <div className="">
+                                    <div className="card" key={index}>
+                                        <img src={event.mobile_cover_picture} alt="cover-pic"/>
+                                        <div className="card-body">
+                                            <h4 className="card-title">{event.name} </h4>
+                                            <div className="card-text info">
+                                                <div>
+                                                    {event.registration_start_time}
+                                                </div>
+                                                <div>
+                                                    {event.fees}
+                                                </div>
+                                                <div>
+                                                    {event.venue}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })} */}
+                <div className="container-fluid mt-5">
+                    <div className="row">
+                        <div className="col-12 col-lg-6 mb-2">
+                            <div className="card">
+                                <img alt="cover-pic"></img>
+                                <div className="card-body">
+                                    <h4 className="card-title">name</h4>
+                                    <ul className="info">
+                                        <li>starts on</li>
+                                        <li> entry fee</li>
+                                        <li>venue</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-lg-6 mb-2">
+                            <div className="card">
+                                <img alt="cover-pic"></img>
+                                <div className="card-body">
+                                    <h4 className="card-title">name</h4>
+                                    <ul className="info">
+                                        <li>starts on</li>
+                                        <li> entry fee</li>
+                                        <li>venue</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-lg-6 mb-2">
+                            <div className="card">
+                                <img alt="cover-pic"></img>
+                                <div className="card-body">
+                                    <h4 className="card-title">name</h4>
+                                    <ul className="info">
+                                        <li>starts on</li>
+                                        <li> entry fee</li>
+                                        <li>venue</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-lg-6 mb-2">
+                            <div className="card">
+                                <img alt="cover-pic"></img>
+                                <div className="card-body">
+                                    <h4 className="card-title">name</h4>
+                                    <ul className="info">
+                                        <li>starts on</li>
+                                        <li> entry fee</li>
+                                        <li>venue</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>              
             </div>
          );
     }
